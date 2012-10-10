8a6ddb1084219c3c1a8c15ece8ea0af754377d6a
Tanner Smith <tanner@tssoftware.net>
2012-10-10 19:25:11 -0400

    Implement tentGetAPIRoots

--- shared/js/tent.js
+++ shared/js/tent.js
@@ -3,6 +3,7 @@ function tentGetPosts(server) {
 }
 
 function tentGetAPIRoots(server) {
+	// Get links from server
 	http = new XMLHttpRequest();
 	http.open('GET', 'https://' + server, false);
 	http.setRequestHeader('Content-Type', 'application/vnd.tent.v0+json');
@@ -10,7 +11,52 @@ function tentGetAPIRoots(server) {
 
 	var headers = http.getAllResponseHeaders();
 
-	console.log(extractHeaders(headers));
+	headers = extractHeaders(headers);
+
+	links = headers['Link'];
+
+	if (links == null || links == '') {
+		console.error('Unable to get links from server.');
+
+		return null;
+	}
+
+	var link_split = links.split(',\s*');
+
+	for (var i = 0; i < link_split.length; i++) {
+		var link = link_split[i];
+
+		var info = link.match(/<([^>]+)>; rel="(https?:\/\/[^\"]+)"\s*$/);
+
+		if (info == null || info.length == 0) {
+			continue;
+		}
+
+		var href = info[1];
+		var rel = info[2];
+
+		if (rel != 'https://tent.io/rels/profile') {
+			continue;
+		}
+
+		// Request from the link href
+		http = new XMLHttpRequest();
+		http.open('GET', href, false);
+		http.send(null);
+
+		if (http.status == 404) {
+			continue;
+		}
+
+		// If the response is JSON, then we've got the servers list
+		var response = JSON.parse(http.response);
+
+		if (response == null) {
+			continue;
+		}
+
+		return response['https://tent.io/types/info/core/v0.1.0']['servers'];
+	}
 
 	return null;
 }
