dd151ce32a3ee6a1c3bbc376de66a3afb29ec24d
Tanner Smith <tanner@tssoftware.net>
2012-10-10 19:59:35 -0400

    Don't call tentGetAPIRoots. Add limit for getLatestPosts

--- browserAction/js/main.js
+++ browserAction/js/main.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
 	chrome.storage.sync.get(['server'], function(data) {
 		if (data.server) {
-			// Get the API roots for the server
-			console.log(tentGetAPIRoots(data.server));
-
 			// Get the latest posts for the server
-			// console.log(tentGetPosts(data.server));
+			console.log(tentGetLatestPosts(data.server, 10));
 		}
 	});
 });
